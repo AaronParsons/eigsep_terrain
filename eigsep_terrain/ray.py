a266b0fb462f5bbec6115c2d4f42335a839e46b5
Aaron Parsons <aparsons@berkeley.edu>
2025-09-19 17:11:10 -0700

    Updates to imports and ray tracing

--- eigsep_terrain/ray.py
+++ eigsep_terrain/ray.py
@@ -8,8 +8,14 @@ from .utils import distance
 dtype_r = np.float32
 dtype_i = np.int32
 
-def ray_trace_basic(E, N, U, start_point, ray=None, nside=None, delta_r_m=1,
-                        r_max=None, dtype=dtype_r):
+def healpix_rays(nside, dtype=dtype_r):
+    npix = healpy.nside2npix(nside)
+    px = np.arange(npix)
+    rays = np.array(healpy.pix2vec(nside, px), dtype=dtype)
+    return rays
+
+def ray_trace_basic(E, N, U, start_point, rays, delta_r_m=1,
+                    r_start=None, max_iter=None, r_max=None, dtype=dtype_r):
     '''Return the distance along a HealPix grid of specified nside from a
     ENU starting point until a ray intersects the terrain, in steps of 
     delta_r_m [m], out to a specified r_max_m (or map edge, if None).
@@ -17,42 +23,44 @@ def ray_trace_basic(E, N, U, start_point, ray=None, nside=None, delta_r_m=1,
     as these points are assumed not to intersect terrain. Returns distance
     [m] in HealPix order, with non-intersecting points set to NaN.'''
 
-    if r_max is None:
-        corners = np.array([[E[0],  N[0],  U[0, 0]],
-                            [E[0],  N[-1], U[-1, 0]],
-                            [E[-1], N[0],  U[0, -1]],
-                            [E[-1], N[-1], U[-1, -1]]], dtype=dtype)
-        r_max = np.linalg.norm(corners - start_point[None, :], axis=1).max()
-    r_max = dtype(r_max)
+    E = np.asarray(E, dtype=dtype)
+    N = np.asarray(N, dtype=dtype)
+    U = np.asarray(U, dtype=dtype, order='C')
+    start_point = np.asarray(start_point, dtype=dtype)
+    rays = np.asarray(rays, dtype=dtype)
+    delta_r_m = dtype(delta_r_m)
     u_max = dtype(U.max())
-    res = E[1] - E[0]
-    assert np.abs(res - (N[1] - N[0])) < 1e-5
-    max_iter = int(np.ceil(r_max / delta_r_m))
-    if ray is None:
-        assert nside is not None
-        npix = healpy.nside2npix(nside)
-        px = np.arange(npix)
-        ray = np.array(healpy.pix2vec(nside, px))
+    dE = E[1] - E[0]
+    dN = N[1] - N[0]
+    if max_iter is None:
+        if r_max is None:
+            corners = np.array([[E[0],  N[0],  U[0, 0]],
+                                [E[0],  N[-1], U[-1, 0]],
+                                [E[-1], N[0],  U[0, -1]],
+                                [E[-1], N[-1], U[-1, -1]]], dtype=dtype)
+            r_max = np.linalg.norm(corners - start_point[None, :], axis=1).max()
+        max_iter = int(np.ceil(r_max / delta_r_m))
+    assert rays.shape[0] == 3
+    dr_vec = delta_r_m * rays
+    if r_start is None:
+        r = np.full(rays.shape[1:], delta_r_m, dtype=dtype)
     else:
-        assert ray.shape[0] == 3
-        ray /= np.linalg.norm(ray, axis=0)
-    dr_vec = (delta_r_m * ray).astype(dtype)
-    r = delta_r_m * np.ones(npix, dtype=dtype)
-    active = np.ones(npix, dtype=bool)
+        r = np.asarray(r_start, dtype=dtype)
+    active = ~np.isnan(r)
     inds = np.flatnonzero(active)
-    r[~active] = np.nan
-    points_m = start_point[:, None] + dr_vec[:, inds]
+    points_m = start_point[:, None] + r[None, inds] * rays[:, inds]
     for i in range(2, max_iter):
-        e_px = np.floor((points_m[0] - E[0]) / res).astype(dtype_i)
-        n_px = np.floor((points_m[1] - N[0]) / res).astype(dtype_i)
+        e_px = np.floor((points_m[0] - E[0]) / dE).astype(dtype_i)
+        n_px = np.floor((points_m[1] - N[0]) / dN).astype(dtype_i)
         u_m = U[n_px, e_px]
         # prune to points that are above ground
         active = (points_m[2] > u_m)
         inds = inds[active]
+        #print(i, np.sum(active), inds[:10])
         if inds.size == 0:
             break
         # take a step along the ray
-        r[inds] = i * delta_r_m
+        r[inds] += delta_r_m
         points_m = points_m[:, active] + dr_vec[:, inds]
         # check if out of bounds
         active  = (u_m[active] < u_max)
