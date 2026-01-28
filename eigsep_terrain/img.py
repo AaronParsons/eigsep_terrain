Fixed bug in ant_loss calculation in img.py

eigsep_terrain/img.py
@@ -20,6 +20,7 @@ def pixels_to_rays(Nu, Nv, f, uv=None, dtype=np.float32):
     rays /= np.linalg.norm(rays, axis=0)
     return rays
 
+
 class HorizonImage:
     def __init__(self, filename, meta={}, **kwargs):
         self.filename = filename
@@ -154,8 +155,7 @@ class HorizonImage:
         
         delta_theta = np.arccos(np.dot(ant_ray, r_ant) / (np.linalg.norm(ant_ray) * np.linalg.norm(r_ant))) # rad
         sigma_theta = box_size / np.linalg.norm(r_ant)
-        
-        logL = -delta_theta / (2 * sigma_theta**2) # :0
+        logL = -delta_theta**2 / (2 * sigma_theta**2)
         return logL
     
 class PositionSolver:
@@ -188,13 +188,15 @@ class PositionSolver:
         self.sigmas = [img.prms[k] * sig if sig == 'f' else sig for k, sig in zip(PRM_ORDER, img_sigmas) for img in self.fit_imgs]
         self.sigmas += [pos_err, pos_err, pos_err]
 
-    def total_loss(self, theta):
+    def total_loss(self, theta, ray_cnt=None):
+        if ray_cnt == None:
+            ray_cnt = self.n_rays
         self.set_mcmc_prms(theta)
         L = 0.0
         for cnt, img in enumerate(self.fit_imgs):
-            L += img.horizon_ray_loss(self.dem, cnt=self.n_rays)
+            L += img.horizon_ray_loss(self.dem, cnt=ray_cnt)
         L = np.array(L / (len(self.fit_imgs) + self.eps), dtype=np.float32)
-        logp = len(self.fit_imgs) * self.n_rays * (1 - L) * np.log(1.0 - self.eps) + len(self.fit_imgs) * self.n_rays * L * np.log(self.eps)
+        logp = len(self.fit_imgs) * ray_cnt * (1 - L) * np.log(1.0 - self.eps) + len(self.fit_imgs) * ray_cnt * L * np.log(self.eps)
         for img in self.imgs:
             logL = img.ant_loss(self.ant_pos, self.box_size)
             logp += logL